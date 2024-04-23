4826d243ccab286bc2dd28ef486d4b60c8171305
eoxe316 <109499021+eoxe316@users.noreply.github.com>
2024-04-24 10:10:04 +1200

    Read sensor function created and basic code template made

---
+++ read_sensors.ino
@@ -0,0 +1,38 @@
+/**INITIALISATION OF VARIABLES*/
+#include <SoftwareSerial.h>
+
+// Serial Data Pins
+#define BLUETOOTH_RX 10
+#define BLUETOOTH_TX 11
+
+// USB Serial Port
+#define OUTPUTMONITOR 0
+#define OUTPUTPLOTTER 0
+
+// Bluetooth Serial Port
+#define OUTPUTBLUETOOTHMONITOR 1
+
+SoftwareSerial BluetoothSerial(BLUETOOTH_RX, BLUETOOTH_TX);
+
+void setup(void)
+{
+  BluetoothSerial.begin(115200);
+  BluetoothSerial.println("");
+  BluetoothSerial.println("");
+  BluetoothSerial.println("");
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  //Initialise Sensor Pins
+  pinMode(A4, INPUT);
+  delay(1000); //settling time but noT really needed
+}
+
+
+/*******************SUPER LOOP**********************/
+void loop(void) //main loop
+{
+    BluetoothSerial.print("Pin reading:");
+    BluetoothSerial.println(analogRead(A4));
+    
+    BluetoothSerial.println("");
+}
